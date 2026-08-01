b84b98177ff3396cccdb0d29c5e0aa7dfe446464
Adedayo Adetoye <3177640+adedayo@users.noreply.github.com>
2026-08-01 14:41:08 +0100

    refactor(suite): convert checkmate-app to submodule pointing to https://github.com/adedayo/checkmate-app

--- checkmate-app/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "=== Running Go Backend Tests ==="
-go test -v .
-
-echo ""
-echo "=== Running Angular Frontend Tests ==="
-cd frontend && npm run test -- --watch=false
